2d2d7d6652c2277cb93f3f32e18a9c28d961af91
CeciliaMichalek <ceciliamichalek@gmail.com>
2023-04-22 20:41:34 -0600

    jsUpdate

--- script/script.js
+++ script/script.js
@@ -1,11 +1,16 @@
 //Current Year
-function year() {
+/*function year() {
     let d = new Date();
     let b = d.getFullYear();
     document.getElementById("copyrightYear").innerHTML = b;
-}
+}*/
 
-function lastUpdated() {
+let date =  new Date().getFullYear();
+document.getElementById("copyrightYear").innerHTML = date;
+
+/*function lastUpdated() {
     let a = document.lastModified;
     document.getElementById("lastUpdated").innerHTML = a;
-}
+}*/
+let currentYear = document.lastModified;
+document.getElementById("lastUpdated").innerHTML = currentYear;
