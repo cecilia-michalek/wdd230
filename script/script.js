978b894120acceca29e2a34c1a8fe2ba333854f3
CeciliaMichalek <ceciliamichalek@gmail.com>
2023-04-22 19:28:20 -0600

    finished css

--- script/script.js
+++ script/script.js
@@ -1,30 +1,6 @@
-//Current Date
-const daynames = [
-	"Sunday",
-	"Monday",
-	"Tuesday",
-	"Wednesday",
-	"Thursday",
-	"Friday",
-	"Saturday"
-];
-const months = [
-	"January",
-	"February",
-	"March",
-	"April",
-	"May",
-	"June",
-	"July",
-	"August",
-	"September",
-	"October",
-	"November",
-	"December"
-];
-const d = new Date();
-const dayName = daynames[d.getDay()];
-const monthName = months[d.getMonth()];
-const year = d.getFullYear();
-const fulldate = `${dayName}, ${d.getDate()} ${monthName} ${year}`;
-document.querySelector("#date2").textContent = fulldate;
+//Current Year
+let fullYear = new Date(document.getFullYear)
+document.querySelector("#copyrightYear").textContent = new Date.getFullYear();
+
+let lastUpdated = new Date(document.lastModified);
+document.querySelector("#lastUpdated").textContent = document.lastUpdated;
