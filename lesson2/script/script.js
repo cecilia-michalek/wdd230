79abb3938263a2d2f307d44c2d69894f9e12b203
CeciliaMichalek <ceciliamichalek@gmail.com>
2023-04-26 08:38:14 -0600

    designPrinciples

---
+++ lesson2/script/script.js
@@ -0,0 +1,5 @@
+let date =  new Date().getFullYear();
+document.getElementById("copyrightYear").innerHTML = date;
+
+let currentYear = document.lastModified;
+document.getElementById("lastUpdated").innerHTML = currentYear;
