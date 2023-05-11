5451fc7e7e9d2be6d127888af6d28f4db9a1e35c
CeciliaMichalek <ceciliamichalek@gmail.com>
2023-05-11 14:33:05 -0600

    lesson4

---
+++ chamber/scripts/date.js
@@ -0,0 +1,16 @@
+
+/*function current year() {
+    let d = new Date();
+    let b = d.getFullYear();
+    document.getElementById("copyrightYear").innerHTML = b;
+}*/
+
+let date =  new Date().getFullYear();
+document.getElementById("copyrightYear").innerHTML = date;
+
+/*function lastUpdated() {
+    let a = document.lastModified;
+    document.getElementById("lastUpdated").innerHTML = a;
+}*/
+let currentYear = document.lastModified;
+document.getElementById("lastUpdated").innerHTML = currentYear;
