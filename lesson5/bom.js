2a053c2dda26785c21f559bba98d9dd8e77a9728
CeciliaMichalek <ceciliamichalek@gmail.com>
2023-05-18 12:25:13 -0600

    lesson5-BoM

---
+++ lesson5/bom.js
@@ -0,0 +1,30 @@
+const list = document.querySelector('ul');
+      const input = document.querySelector('input');
+      const button = document.querySelector('button');
+      
+      button.onclick = function (){
+        let newItem = input.value;
+        input.value = '';
+        
+        let itemList = document.createElement('li');
+        let itemText = document.createElement('span');
+        let itemBtn = document.createElement('button');
+        
+        itemList.appendChild(itemText);
+        itemText. textContent = newItem;
+        
+        itemList.appendChild(itemBtn);
+        itemBtn. textContent = '❌';
+        
+        list.appendChild(itemList);
+        
+        itemBtn.onclick = function () {
+          list.removeChild(itemList);
+        }
+        
+        input.focus();
+        
+        
+      }
+
+      
