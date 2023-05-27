07cae3bafaba1bd9d393e1a61bbc3c72f430d16a
CeciliaMichalek <ceciliamichalek@gmail.com>
2023-05-27 10:37:57 -0600

    bomCorrections

--- lesson5/bom.js
+++ lesson5/bom.js
@@ -1,43 +1,72 @@
-const list = document.querySelector('ul');
-const input = document.querySelector('myInput');
-const button = document.querySelector('myBtn');
+//Book of Mormon Chapter list
 
-button.disabled = true;
-     
-button.addEventListener("click", function()) {
-  let val = event.target.value;
+const button = document.getElementById("button");
+const input = document.getElementById("input");
+const ul = document.querySelector("ul");
+const del = document.getElementsByClassName("delBtn");
 
-    if(val==='') {
-      button.disabled = true;  // Make button disabled
-  }
-  else{
-      button.disabled = false;  // Make button enabled 
-  }
-  button.onclick = function (){
-    let newItem = input.value;
-    input.value = '';  
 
-    let itemList = document.createElement('li');
-    let itemText = document.createElement('span');
-    let itemBtn = document.createElement('button');
-        
-    itemList.appendChild(itemText);
-    itemText. textContent = newItem;
+
+function inputLength() {
+	return input.value.length;
+}
+
+function createListElement() {
+	let li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
   
-        
-    itemList.appendChild(itemBtn);
-    itemBtn. textContent = '❌';
-        
-    list.appendChild(itemList);
-        
-    itemBtn.onclick = function () {
-    list.removeChild(itemList);
-        }
-    
-    
-    input.focus();
-        
-        
-      }
-    }
+	//creates a button and adds it inside the li
+	let btn = document.createElement("Button");
+	btn.appendChild(document.createTextNode("❌"));
+	btn.classList.add('delBtn');
+	li.appendChild(btn);
+	ul.appendChild(li);
+	setButtons();
+	input.value = "";
+}
+
+function addListAfterClick() {
+	if (inputLength() > 0) {
+		createListElement();
+	}
+}
+
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.keyCode === 13) {
+		createListElement();
+	}
+}
+
+//toggles done class to strikethrough
+function liClicked()
+{
+	console.log("li clicked");
+	let element = document.querySelector("li");
+  	element.classList.toggle("done");
+}
+
+//removes the li as parent element
+function deleteLi(e){
+	e.target.parentNode.remove();
+
+}
+
+function toggleStrike(e) {
+		if (e.target.tagName === 'LI')
+		{
+      	e.target.classList.toggle("done");
+  	}
+}
+
+//adds event listener to all buttons
+function setButtons(){
+	for (var i = 0; i <del.length; i++){
+		del[i].addEventListener('click', deleteLi);
+	}
+}
+
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
+
       
