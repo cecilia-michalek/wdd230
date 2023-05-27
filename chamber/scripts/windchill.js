fbb833b003417abfff9f66c435ff080476db924b
CeciliaMichalek <ceciliamichalek@gmail.com>
2023-05-27 09:50:54 -0600

    windchill1

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,28 +1,12 @@
 
 
-document.addEventListener("DOMContentLoader", function() {
-    const s = 25;
-    const t = 30;
-    let windchill = document.getElementById("windchill");
-
-    windchill.innerHTML = buildWC(s, t);
-});
-
-function buildWC(s, t) {
-    
-
-
-//Windchill calculation
-    let wc = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16);
-    if (t >= 50 && s > 3.0) {
-        windchill = wc.toFixed(2);
-      } else {
-        windchill = "N/A";
-      }
-      document.getElementById("windChill").innerHTML = windchill;
-
-//Round answer
-    wc = Math.floor(wc);
+const t = parseFloat(document.getElementById("temperature").innerHTML);
+const s = parseFloat(document.getElementById("windspeed").innerHTML);
+const f = 35.74 + 0.6215*t - 35.75*(Math.pow(s,0.16)) + 0.4275*t*(Math.pow(s,0.16))
+if (t<=50 && s>3.0){
+        windchill=f.toFixed(2);
     }
-//windchill
-return wc;
+else{
+    windchill = "N/A";
+}
+document.getElementById('windchill').innerHTML = windchill;
