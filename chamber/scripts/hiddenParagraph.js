9334a60985d4650736b4e0027935d9174f1b84d6
CeciliaMichalek <ceciliamichalek@gmail.com>
2023-06-09 11:45:43 -0600

    join/discoverUpdates

--- chamber/scripts/hiddenParagraph.js
+++ chamber/scripts/hiddenParagraph.js
@@ -1,8 +1,12 @@
-function toggleParagraph() {
-    var paragraph = document.getElementsById("myParagraph1");
+/*function toggleParagraph() {
+    var paragraph = document.getElementsById("myParagraph");
     if (paragraph.style.display === "none") {
       paragraph.style.display = "block";
     } else {
       paragraph.style.display = "none";
     }
+  }*/
+  function toggleParagraph(paragraphId) {
+    var paragraph = document.getElementById(paragraphId);
+    paragraph.classList.toggle('hidden');
   }
