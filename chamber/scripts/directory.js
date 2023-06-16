267bd3fb13354361662973105a8d93d95f048e8e
CeciliaMichalek <ceciliamichalek@gmail.com>
2023-06-16 17:54:59 -0600

    correction

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -2,10 +2,14 @@ const url = 'data.json';
 getDirectoryData();
 
 async function getDirectoryData() {
+  try {
     const response = await fetch(url);
     const data = await response.json();
-    console.table(data.directory);  // note that we reference the prophet array of the data object given the structure of the json file
+    //console.table(data.directory);  // note that we reference the prophet array of the data object given the structure of the json file
     displayDirectory(data.directory);
+  } catch (error) {
+    console.error('Error:', error);
+  }
 }
 
     const displayDirectory = (business) => {
@@ -17,26 +21,28 @@ async function getDirectoryData() {
         let name = document.createElement('h2');
         let location = document.createElement('p');
         let phoneNumber = document.createElement('p');
+        let email = document.createElement('p');
         let imageurl = document.createElement('img');
     
         // Build the h2 content out to show the prophet's full name - finish the template string
-        h2.textContent = `${business.name}`;
-        location.innerHTML = `<strong>Location</strong>: ${business.location}`;
-        phoneNumber.innerHTML = `${prophet.phoneNumber}`;
-        email.innerHTML = `${ business.email}`;
+        name.textContent = `${business.name}`;
+        location.innerHTML = `${business.location}`;
+        phoneNumber.innerHTML = `${business.phoneNumber}`;
+        email.innerHTML = `${business.email}`;
         
         // Build the image portrait by setting all the relevant attribute
-        portrait.setAttribute('src', business.imageurl);
-        portrait.setAttribute('alt', `Portait of ${business.name}`);
-        portrait.setAttribute('loading', 'lazy');
-        portrait.setAttribute('width', '340');
-        portrait.setAttribute('height', '440');
+        imageurl.setAttribute('src', business.imageurl);
+        imageurl.setAttribute('alt', `${business.name}`);
+        imageurl.setAttribute('loading', 'lazy');
+        imageurl.setAttribute('width', '340');
+        imageurl.setAttribute('height', '440');
     
         // Append the section(card) with the created elements
         card.appendChild(name);
         card.appendChild(location);
         card.appendChild(phoneNumber);
         card.appendChild(email);
+        card.appendChild(imageurl);
     
         cards.appendChild(card);
       }); // end of forEach loop
