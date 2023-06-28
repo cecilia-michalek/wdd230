17cd1a55943f7a0f5c0cffee86e28fede75a8c6a
CeciliaMichalek <ceciliamichalek@gmail.com>
2023-06-28 10:01:06 -0600

    fixed2

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -1,4 +1,4 @@
-// Load the JSON data from an external file
+// Load the JSON data from an internal file
 fetch('data.json')
   .then(response => response.json())
   .then(data => {
@@ -26,4 +26,42 @@ const filteredMembers = directoryData.members.filter(function(members) {
     console.log("-----");
   });
 })
-.catch(error => console.error(error));
+
+
+const displayMembers = (members) => {
+  const cards = document.querySelector('div.cards'); // select the output container element
+
+  members.forEach((members) => {
+    // Create elements to add to the div.cards element
+    let card = document.createElement('section');
+    let h2 = document.createElement('h2');
+    let location = document.createElement('p');
+    let email = document.createElement('p');
+    let phoneNumber = document.createElement('p');
+    let logo = document.createElement('img');
+
+    // Build the h2 content out to show the prophet's full name - finish the template string
+    h2.textContent = `${members.name}`;
+    location.innerHTML = `${members.location}`;
+    phoneNumber.innerHTML = `${members.phoneNumber}`;
+    email.innerHTML = `${members.email}`;
+    
+    // Build the image portrait by setting all the relevant attribute
+    logo.setAttribute('src', members.imageurl);
+    logo.setAttribute('alt', `${members.name}`);
+    logo.setAttribute('loading', 'lazy');
+    logo.setAttribute('width', '340');
+    logo.setAttribute('height', '440');
+
+    // Append the section(card) with the created elements
+    card.appendChild(h2);
+    card.appendChild(location);
+    card.appendChild(phoneNumber);
+    card.appendChild(email);
+    card.appendChild(logo);
+
+    cards.appendChild(card);
+  }); // end of forEach loop
+} // end of function expression
+
+
